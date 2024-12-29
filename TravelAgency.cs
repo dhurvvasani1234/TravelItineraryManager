8fcd826397178a6730c7ae24f6e97803592ce6a7
DHRUVVASANI1234 <DHRUVVASANI1234@GMAIL.COM>
2024-12-29 12:03:48 -0500

    first commit

---
+++ TravelAgency.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Midterm
+{
+    class TravelAgency
+    {
+        static void Main(string[] args)
+        {
+            List<Itinerary> itineraries = new List<Itinerary>();
+
+            Console.WriteLine("Welcome to Algonquin College Student Travel Agency!");
+
+            Console.Write("\nEnter V to view all itineraries, A to add a new itinerary. \n" +
+                            "      C to change an existing itinerary, and E to exit: ");
+            string action = Console.ReadLine();
+            Console.WriteLine();
+
+                 
+            //Write your code here to implement the functionality as required
+
+
+
+            Console.WriteLine("\nThank you for using Algonquin College Student Travel Agency!");
+            Console.WriteLine("Press return key to complete the application");
+            Console.ReadLine();
+        }
+    }
+}
